20133eada5f4e3829a8b04b3caccf8c43c004a05
Gaye Yılmaz <gaye3545@gmail.com>
2022-03-20 19:24:06 +0300

    DestroyOutOfBound, Dog Prefab Collider modified

--- Challenge2-PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenge2-PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float timer = 1.0f;
+    private float waitTime = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Delay Input Timer - only execute the spacebar command if timer has caught up with waitTime
+        if (timer < waitTime)
+        { }else if (Input.GetKeyDown(KeyCode.Space))
         {
-         
-            Instantiate(dogPrefab, dogPrefab.transform.position, dogPrefab.transform.rotation);
+            SpawnDogPrefab();
+            timer = 0;
         }
+        
+        // Run Timer every frame
+        timer += Time.deltaTime;
+        
+    }
+
+
+
+    void SpawnDogPrefab()
+    {
+        Instantiate(dogPrefab, dogPrefab.transform.position, dogPrefab.transform.rotation);
     }
 }
--- Challenge2-PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge2-PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,14 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float startInterval = 3;
+    private float endInterval = 5; 
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        //Generates random interval for the spawn ball
+       InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(startInterval, endInterval));
     }
 
     // Spawn random ball at random x position at top of play area
@@ -24,7 +26,7 @@ public class SpawnManagerX : MonoBehaviour
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
-
+    
         //Choose the ball randomly
         int ballIndex = Random.Range(0, ballPrefabs.Length);
         // instantiate ball at random spawn location
